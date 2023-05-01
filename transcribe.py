f1abb2eac059e99ef4261411be71f89a8e2afc71
Rudranil Bhattacharjee <84077013+rudranil03822@users.noreply.github.com>
2023-05-01 15:26:18 +0530

    Add files via upload
    All files added

---
+++ transcribe.py
@@ -0,0 +1,71 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+import os
+import openai
+from dotenv import load_dotenv, find_dotenv
+
+# Using OpenAI API
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+def get_completion(prompt, model="gpt-3.5-turbo"): 
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+
+def extract_ideas(vid_id):
+    srt = YouTubeTranscriptApi.get_transcript("7x5M4lxK-dw")
+    # Creating an empty list to store ideas scrapped from parts of the video
+    ideas = [] 
+    # Splitting the entrire into batches so that doesn't exceed openai api limit
+    for i in range(1,(len(srt)//100)+1):
+        print(i)
+        s1= srt[100*(i-1):i*100]
+        prompt = f"""
+        Your task is to summarise all business ideas being discussed in the YouTube video \
+        from the transcript of the video. 
+        Make sure you read through the entire transcript first and then summarise.
+        The format should be
+        Idea 1: ......
+        Idea 2: ......
+        .
+        .
+        Idea N: .....
+        Summarize the transcript given below, delimited by triple 
+        backticks. 
+        output as a list
+        transcript: ```{s1}```
+        """
+
+        response = get_completion(prompt)
+        ideas.append(response)
+    # Secondary prompt for 
+    prompt1 = f"""
+    Your task is to summarise all the business into full business ideas based on the \
+    list of ideas given. Make sure first you go through all the ideas, then combine all the ideas\
+    that are for same category and write them as one idea after summeririzing them.
+    The format should be
+    Idea 1 (Title of idea): Description of that entire idea
+    Idea 2 (Title of idea): Description of that entire idea
+    .
+    .
+    Idea N (Title of idea): Description of that entire idea
+    Get the list of ideas given below, delimited by triple 
+    backticks. 
+    list ideas: ```{ideas}```
+    """
+
+    response1 = get_completion(prompt1)
+    return response1
+
+# Define the video ID for the YouTube video
+video_id = "7x5M4lxK-dw"
+# Calling the function defined above
+list_idea = extract_ideas(video_id)
+print(list_idea)
